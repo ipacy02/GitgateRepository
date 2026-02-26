8290d85f57293eae0531ad1344fa1d182c3a8257
ipacy02 <ipacy02@gmail.com>
2026-02-26 17:48:54 +0200

    new change

--- project2/src/main/java/org/example/test.java
+++ project2/src/main/java/org/example/test.java
@@ -7,7 +7,7 @@ package org.example;
         public static void main(String[] args) {
 
             // TODO: Implement business logic
-            System.out.println("Work in progress and it is working properly");
+            System.out.println("We are working on the main and want to change");
 
             // Placeholder method call
             greetUser("Developer");
